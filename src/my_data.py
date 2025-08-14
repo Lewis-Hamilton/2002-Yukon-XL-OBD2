08d56f1028ac59adf61fb0032d0f75752d3ba8cc
Lewis <hamiltonjlewis@gmail.com>
2025-08-13 20:41:45 -0500

    reorganized data in alphabetical order

--- src/my_data.py
+++ src/my_data.py
@@ -22,196 +22,196 @@ class ObdData:
         myresponse = connection.query(self.cmd)
         return myresponse
 
-coolant_temp = ObdData(
-    name="Coolant Tempurature",
-    cmd=obd.commands.COOLANT_TEMP,
-    unit="Celsius"
-)
-
-elm_voltage = ObdData(
-    name="Voltage",
-    cmd=obd.commands.ELM_VOLTAGE,
-    unit="Volts"
-)
-
-speed = ObdData(
-    name="Speed",
-    cmd=obd.commands.SPEED,
-    unit="Kilometers"
-)
-
-rpm = ObdData(
-    name="RPM",
-    cmd=obd.commands.RPM,
-    unit="RPM"
-)
-
-throttle_pos = ObdData(
-    name="Throttle Position",
-    cmd=obd.commands.THROTTLE_POS,
-    unit="Unkown"
-)
-
-intake_temp = ObdData(
-    name="Intake Tempurature",
-    cmd=obd.commands.INTAKE_TEMP,
-    unit="Celsius"
-)
-
-engine_load = ObdData(
-    name="Engine Load",
-    cmd=obd.commands.ENGINE_LOAD,
-    unit=" Percentage"
-)
-
-aux_input = ObdData(
+AUX_INPUT_STATUS = ObdData(
     name="Auxillary Input",
     cmd=obd.commands.AUX_INPUT_STATUS,
     unit="Unkown"
 )
 
-o2_b1s1 = ObdData(
-    name="O2 Bank 1 Sensor 1",
-    cmd=obd.commands.O2_B1S1,
-    unit="Volts"
+COOLANT_TEMP = ObdData(
+    name="Coolant Tempurature",
+    cmd=obd.commands.COOLANT_TEMP,
+    unit="Celsius"
 )
 
-o2_b1s2 = ObdData(
-    name="O2 Bank 1 Sensor 2",
-    cmd=obd.commands.O2_B1S2,
-    unit="Volts"
+ENGINE_LOAD = ObdData(
+    name="Engine Load",
+    cmd=obd.commands.ENGINE_LOAD,
+    unit=" Percentage"
 )
 
-o2_b2s1 = ObdData(
-    name="O2 Bank 2 Sensor 1",
-    cmd=obd.commands.O2_B2S1,
-    unit="Volts"
-)
-
-o2_b2s2 = ObdData(
-    name="O2 Bank 2 Sensor 2",
-    cmd=obd.commands.O2_B2S2,
-    unit="Volts"
-)
-
-o2_sensors = ObdData(
-    name="O2 Sensors Status",
-    cmd=obd.commands.O2_SENSORS,
-    unit="Unkown"
-)
-
-timing = ObdData(
-    name="Timing Advance",
-    cmd=obd.commands.TIMING_ADVANCE,
-    unit="Degrees"
-)
-
-# maybe display this on startup
-version = ObdData(
-    name="Firmware Version",
-    cmd=obd.commands.ELM_VERSION,
-    unit="Unkown"
-)
-
-maf = ObdData(
-    name="Mass Airflow Sensor",
-    cmd=obd.commands.MAF,
-    unit="Unkown"
-)
-
-intake_pressure = ObdData(
-    name="Intake Pressure",
-    cmd=obd.commands.INTAKE_PRESSURE,
-    unit="kilopascal"
-)
-
-b1_short_fuel_trim = ObdData(
-    name="Bank 1 Short Fuel Trim",
-    cmd=obd.commands.SHORT_FUEL_TRIM_1,
-    unit="Unkown"
-)
-
-b2_short_fuel_trim = ObdData(
-    name="Bank 2 Short Fuel Trim",
-    cmd=obd.commands.SHORT_FUEL_TRIM_2,
-    unit="Unkown"
-)
-
-b1_long_fuel_trim = ObdData(
-    name="Bank 1 Long Fuel Trim",
-    cmd=obd.commands.LONG_FUEL_TRIM_1,
-    unit="Percentage"
-)
-
-b2_long_fuel_trim = ObdData(
-    name="Bank 2 Long Fuel Trim",
-    cmd=obd.commands.LONG_FUEL_TRIM_2,
-    unit="Percentage"
-)
-
-fuel_status = ObdData(
+FUEL_STATUS = ObdData(
     name="Fuel Status",
     cmd=obd.commands.FUEL_STATUS,
     unit="Unkown"
 )
 
-obd_status = ObdData(
-    name="OBD Status",
-    cmd=obd.commands.STATUS,
+INTAKE_PRESSURE = ObdData(
+    name="Intake Pressure",
+    cmd=obd.commands.INTAKE_PRESSURE,
+    unit="kilopascal"
+)
+
+INTAKE_TEMP = ObdData(
+    name="Intake Tempurature",
+    cmd=obd.commands.INTAKE_TEMP,
+    unit="Celsius"
+)
+
+LONG_FUEL_TRIM_1 = ObdData(
+    name="Bank 1 Long Fuel Trim",
+    cmd=obd.commands.LONG_FUEL_TRIM_1,
+    unit="Percentage"
+)
+
+LONG_FUEL_TRIM_2 = ObdData(
+    name="Bank 2 Long Fuel Trim",
+    cmd=obd.commands.LONG_FUEL_TRIM_2,
+    unit="Percentage"
+)
+
+MAF = ObdData(
+    name="Mass Airflow Sensor",
+    cmd=obd.commands.MAF,
     unit="Unkown"
 )
 
-pids_a = ObdData(
-    name="PIDS A",
-    cmd=obd.commands.PIDS_A,
-    unit="Unkown"
-)
-
-pids_9a = ObdData(
-    name="PIDS 9A",
-    cmd=obd.commands.PIDS_9A,
-    unit="Unkown"
-)
-
-obd_compliance = ObdData(
-    name="OBD Compliance",
-    cmd=obd.commands.OBD_COMPLIANCE,
-    unit="Unkown"
-)
-
-mids_a = ObdData(
+MIDS_A = ObdData(
     name="MIDS A",
     cmd=obd.commands.MIDS_A,
     unit="Unkown"
 )
 
+O2_B1S1 = ObdData(
+    name="O2 Bank 1 Sensor 1",
+    cmd=obd.commands.O2_B1S1,
+    unit="Volts"
+)
+
+O2_B1S2 = ObdData(
+    name="O2 Bank 1 Sensor 2",
+    cmd=obd.commands.O2_B1S2,
+    unit="Volts"
+)
+
+O2_B2S1 = ObdData(
+    name="O2 Bank 2 Sensor 1",
+    cmd=obd.commands.O2_B2S1,
+    unit="Volts"
+)
+
+O2_B2S2 = ObdData(
+    name="O2 Bank 2 Sensor 2",
+    cmd=obd.commands.O2_B2S2,
+    unit="Volts"
+)
+
+O2_SENSORS = ObdData(
+    name="O2 Sensors Status",
+    cmd=obd.commands.O2_SENSORS,
+    unit="Unkown"
+)
+
+OBD_COMPLIANCE = ObdData(
+    name="OBD Compliance",
+    cmd=obd.commands.OBD_COMPLIANCE,
+    unit="Unkown"
+)
+
+PIDS_9A = ObdData(
+    name="PIDS 9A",
+    cmd=obd.commands.PIDS_9A,
+    unit="Unkown"
+)
+
+PIDS_A = ObdData(
+    name="PIDS A",
+    cmd=obd.commands.PIDS_A,
+    unit="Unkown"
+)
+
+RPM = ObdData(
+    name="RPM",
+    cmd=obd.commands.RPM,
+    unit="RPM"
+)
+
+SHORT_FUEL_TRIM_1 = ObdData(
+    name="Bank 1 Short Fuel Trim",
+    cmd=obd.commands.SHORT_FUEL_TRIM_1,
+    unit="Unkown"
+)
+
+SHORT_FUEL_TRIM_2 = ObdData(
+    name="Bank 2 Short Fuel Trim",
+    cmd=obd.commands.SHORT_FUEL_TRIM_2,
+    unit="Unkown"
+)
+
+SPEED = ObdData(
+    name="Speed",
+    cmd=obd.commands.SPEED,
+    unit="Kilometers"
+)
+
+STATUS = ObdData(
+    name="OBD Status",
+    cmd=obd.commands.STATUS,
+    unit="Unkown"
+)
+
+THROTTLE_POSITION = ObdData(
+    name="Throttle Position",
+    cmd=obd.commands.THROTTLE_POS,
+    unit="Unkown"
+)
+
+TIMING_ADVANCE = ObdData(
+    name="Timing Advance",
+    cmd=obd.commands.TIMING_ADVANCE,
+    unit="Degrees"
+)
+# maybe display this on startup
+VERSION = ObdData(
+    name="Firmware Version",
+    cmd=obd.commands.ELM_VERSION,
+    unit="Unkown"
+)
+
+VOLTAGE = ObdData(
+    name="Voltage",
+    cmd=obd.commands.ELM_VOLTAGE,
+    unit="Volts"
+)
+
 all_data = [
-    # o2_sensors,
-    # version,
-    maf,
-    b1_short_fuel_trim,
-    b2_short_fuel_trim,
-    fuel_status,
-    # obd_status,
-    # pids_a,
-    # pids_9a,
-    # obd_compliance,
-    # mids_a,
-    coolant_temp,
-    intake_temp,
-    b1_long_fuel_trim,
-    b2_long_fuel_trim,
-    intake_pressure,
-    timing,
-    engine_load,
-    o2_b1s1,
-    o2_b1s2,
-    o2_b2s1,
-    o2_b2s2,
-    elm_voltage,
-    speed,
-    rpm,
-    throttle_pos,
+    # AUX_INPUT_STATUS,
+    COOLANT_TEMP,
+    ENGINE_LOAD,
+    FUEL_STATUS,
+    INTAKE_PRESSURE,
+    INTAKE_TEMP,
+    LONG_FUEL_TRIM_1,
+    LONG_FUEL_TRIM_2,
+    MAF,
+    # MIDS_A,
+    O2_B1S1,
+    O2_B1S2,
+    O2_B2S1,
+    O2_B2S2,
+    # O2_SENSORS,
+    # OBD_COMPLIANCE,
+    # PIDS_9A,
+    # PIDS_A,
+    RPM,
+    SHORT_FUEL_TRIM_1,
+    SHORT_FUEL_TRIM_2,
+    SPEED,
+    # STATUS,
+    THROTTLE_POSITION,
+    TIMING_ADVANCE,
+    # VERSION,
+    VOLTAGE,
 ]
 
 # - DTC_INTAKE_TEMP
