21305e9eb291548ca94a6bba24bbce6364e92505
Lewis <38916472+Lewis-Hamilton@users.noreply.github.com>
2026-06-25 08:28:46 -0500

    fixed initial idle ready

--- src/idle_calc.py
+++ src/idle_calc.py
@@ -4,21 +4,26 @@ from args import parser
 args = parser.parse_args()
 
 IDLE_DELAY = 15
-STOP_DELAY = 7
+STOP_DELAY = 5
 
-_stopped_since = None
+_stopped_since = 0
 _below_idle_since = None
+_prev_speed = 0
 
 def idle_ready(rpm, speed):
-    global _stopped_since, _below_idle_since
+    global _stopped_since, _below_idle_since, _prev_speed
 
     if speed > 0:
         _stopped_since = None
         _below_idle_since = None
+        _prev_speed = speed
         return True
 
-    if _stopped_since is None:
+    # speed == 0 from here on
+    if _prev_speed is None or _prev_speed > 0:
         _stopped_since = time.time()
+
+    _prev_speed = speed
 
     if time.time() - _stopped_since < STOP_DELAY:
         # waiting to slow down, don't check idle
